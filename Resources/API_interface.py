33f624ec54d546245ccf8185e38ff6ad56001171
belangeo <belangeo@47a9a872-8ef4-7ce4-ef87-7645ece8daa9>
2011-11-20 19:35:14 +0000

    Work on API documentation.

--- Resources/API_interface.py
+++ Resources/API_interface.py
@@ -382,8 +382,59 @@ def crange(name="range", label="Pitch", min=20.0, max=20000.0, init=[500.0, 2000
     dic["help"] = help
     return dic
 
-def csplitter(name="splitter", label="Pitch", min=20.0, max=20000.0, init=[500.0, 2000.0, 5000.0], rel="log", num_knobs=3,
-           res="float", gliss=0.025, unit="x", up=False, func=None, midictl=None, col="red", help="text for tooltip"):
+def csplitter(name="splitter", label="Pitch", min=20.0, max=20000.0, init=[500.0, 2000.0, 5000.0], rel="log",
+              res="float", gliss=0.025, unit="x", up=False, num_knobs=3, col="red", help="text for tooltip"):
+    """
+    Description:
+    
+    When created, the splitter is stacked in the slider pane of the main 
+    Cecilia window in the order it is defined. The values of the splitter 
+    slider are passed to the module with a variable named `self.name`. The 
+    knob values are collected using `self.name[0]` to `self.name[num-knobs-1]`.
+    The `up` argument passes the values of the splitter on mouse-up if set to 
+    True or continuously if set to False. The `gliss` argument determines the 
+    duration of the portamento (in sec) applied on new values. The resolution 
+    of the splitter slider can be set to int or float using the `res` argument. 
+    The slider colour can be set using the `col` argument and a colour value. 
+    However, sliders with `up` set to True are greyed and the `col` argument 
+    is ignored.
+
+    The csplitter is especially designed to be used with the FourBand object in
+    order to do multi-band processing. Although the FourBand parameters can be
+    changed at audio rate, it is not recommended. This filter is CPU expensive 
+    and can have erratic behavior when boudaries are changed to quickly.
+
+    Parameters:
+    
+        name : str
+            Name of the splitter slider.
+        label : str
+            Label shown in the csplitter's label.
+        min : float
+            Minimum value of the splitter slider.
+        max : float
+            Maximum value of the splitter slider.
+        init : list of float
+            Splitter knobs initial values. List must be of length `num_knobs`.
+            Defaults to [500.0, 2000.0, 5000.0].
+        rel : str {"lin", "log"}
+            Splitter slider scaling. Defaults to "lin".
+        res : str {"int", "float"}
+            Splitter slider resolution. Defaults to "float"
+        gliss : float
+            Portamento between values in seconds. Defaults to 0.025.
+        unit : str
+            Unit symbol shown in the interface.
+        up : boolean
+            Value passed on mouse up if True. Defaults to False.
+        num_knobs : int
+            Number of junction knobs. Defaults to 3.
+        col : str
+            Colour of the widget.
+        help : str
+            Help string shown in the csplitter's tooltip.
+
+    """
     dic = {"type": "csplitter"}
     dic["name"] = name
     dic["label"] = label
@@ -391,16 +442,11 @@ def csplitter(name="splitter", label="Pitch", min=20.0, max=20000.0, init=[500.0
     dic["max"] = max
     dic["init"] = init
     dic["rel"] = rel
-    dic["num_knobs"] = num_knobs
     dic["res"] = res
-    if func == None:
-        dic["func"] = [None for i in range(num_knobs)]
-    else:
-        dic["func"] = func
     dic["gliss"] = gliss
     dic["unit"] = unit
     dic["up"] = up
-    dic["midictl"] = midictl
+    dic["num_knobs"] = num_knobs
     dic["col"] = col
     dic["help"] = help
     return dic
