6aba5e64c853d14721399bc83e79910e0bf25b4b
belangeo <belangeo@47a9a872-8ef4-7ce4-ef87-7645ece8daa9>
2012-01-18 03:56:50 +0000

    Change app name to Cecilia5.

--- trunk/scripts/builder_OSX.sh
+++ trunk/scripts/builder_OSX.sh
@@ -1,22 +1,22 @@
 rm -rf build dist
-py2applet --make-setup Cecilia.py Resources/*
+py2applet --make-setup Cecilia5.py Resources/*
 python setup.py py2app --plist=scripts/info.plist
 rm -f setup.py
 rm -rf build
-mv dist Cecilia4_OSX
+mv dist Cecilia5_OSX
 
-if cd Cecilia4_OSX;
+if cd Cecilia5_OSX;
 then
     find . -name .svn -depth -exec rm -rf {} \
     find . -name *.pyc -depth -exec rm -f {} \
     find . -name .* -depth -exec rm -f {} \;
 else
-    echo "Something wrong. Cecilia_OSX not created"
+    echo "Something wrong. Cecilia5_OSX not created"
     exit;
 fi
 
-rm Cecilia.app/Contents/Resources/Cecilia.ico
-rm Cecilia.app/Contents/Resources/CeciliaFileIcon.ico
+rm Cecilia5.app/Contents/Resources/Cecilia5.ico
+rm Cecilia5.app/Contents/Resources/CeciliaFileIcon5.ico
 cd ..
 #tar -cjvf Cecilia4_OSX-0.1.tar.bz2 Cecilia4_OSX
-#rm -rf Cecilia4_OSX
+#rm -rf Cecilia5_OSX
