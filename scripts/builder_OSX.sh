f67500efe475168ac7d55162f0f66de994144717
belangeo <belangeo@47a9a872-8ef4-7ce4-ef87-7645ece8daa9>
2012-02-19 03:54:48 +0000

    Updated version number to 5.0.3.

--- scripts/builder_OSX.sh
+++ scripts/builder_OSX.sh
@@ -9,7 +9,7 @@ if cd Cecilia5_OSX;
 then
     find . -name .svn -depth -exec rm -rf {} \
     find . -name *.pyc -depth -exec rm -f {} \
-    find . -name .* -depth -exec rm -f {} \;
+    find . -name .* -depth -exec rm -f {} \ ;
 else
     echo "Something wrong. Cecilia5_OSX not created"
     exit;
@@ -24,6 +24,6 @@ mv Cecilia5-i386.app Cecilia5.app
 
 cd ..
 cp -R Cecilia5_OSX/Cecilia5.app .
-tar -cjvf Cecilia5_OSX-5.0.2.tar.bz2 Cecilia5.app
+tar -cjvf Cecilia5_OSX-5.0.3.tar.bz2 Cecilia5.app
 rm -rf Cecilia5_OSX
 rm -rf Cecilia5.app
--- scripts/release_src.sh
+++ scripts/release_src.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-svn export . Cecilia5_5.0.2-src
-tar -cjvf Cecilia5_5.0.2-src.tar.bz2 Cecilia5_5.0.2-src
-rm -R Cecilia5_5.0.2-src
+svn export . Cecilia5_5.0.3-src
+tar -cjvf Cecilia5_5.0.3-src.tar.bz2 Cecilia5_5.0.3-src
+rm -R Cecilia5_5.0.3-src
