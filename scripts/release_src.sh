73431b83051e8d266081718d5735d1251665b85c
belangeo <belangeo@47a9a872-8ef4-7ce4-ef87-7645ece8daa9>
2012-05-11 02:11:14 +0000

    Updated version number to 5.0.5.

--- scripts/release_src.sh
+++ scripts/release_src.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-svn export . Cecilia5_5.0.4-src
-tar -cjvf Cecilia5_5.0.4-src.tar.bz2 Cecilia5_5.0.4-src
-rm -R Cecilia5_5.0.4-src
+svn export . Cecilia5_5.0.5-src
+tar -cjvf Cecilia5_5.0.5-src.tar.bz2 Cecilia5_5.0.5-src
+rm -R Cecilia5_5.0.5-src
