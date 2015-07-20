bad8ce623844d2bf9ef3e1c47754566f554ca8ee
Olivier Belanger <belangeo@gmail.com>
2015-07-20 14:13:17 -0400

    Updated windows and OSX builder scripts.

--- scripts/Build_routine_Win32.py
+++ scripts/Build_routine_Win32.py
@@ -1,19 +1,21 @@
-import os
+import os, sys
 
-build = True
+version = sys.version_info[:2]
 
-os.system("python ..\pyinstaller\Configure.py")
-os.system('python ..\pyinstaller\Makespec.py -F -c --icon=Resources\Cecilia5.ico "Cecilia5.py"')
-if build:
-    os.system('python ..\pyinstaller\Build.py "Cecilia5.spec"')
-    os.system("svn export . Cecilia5_Win")
-    os.system("copy dist\Cecilia5.exe Cecilia5_Win /Y")
-    os.system("rmdir /Q /S Cecilia5_Win\scripts")
-    os.system("rmdir /Q /S Cecilia5_Win\doc-en")
-    os.remove("Cecilia5_Win/Cecilia5.py")
-    os.remove("Cecilia5_Win/Resources/Cecilia5.icns")
-    os.remove("Cecilia5_Win/Resources/CeciliaFileIcon5.icns")
-    os.remove("Cecilia5.spec")
-    os.system("rmdir /Q /S build")
-    os.system("rmdir /Q /S dist")
+os.system('C:\Python%d%d\Scripts\pyi-makespec -F -c --icon=Resources\Cecilia5.ico "Cecilia5.py"' % version)
+os.system('C:\Python%d%d\Scripts\pyi-build "Cecilia5.spec"' % version)
+
+os.system("git checkout-index -a -f --prefix=Cecilia5_Win/")
+os.system("copy dist\Cecilia5.exe Cecilia5_Win /Y")
+os.system("rmdir /Q /S Cecilia5_Win\scripts")
+os.system("rmdir /Q /S Cecilia5_Win\doc-en")
+os.remove("Cecilia5_Win/Cecilia5.py")
+os.remove("Cecilia5_Win/Resources/Cecilia5.icns")
+os.remove("Cecilia5_Win/Resources/CeciliaFileIcon5.icns")
+os.remove("Cecilia5.spec")
+os.system("rmdir /Q /S build")
+os.system("rmdir /Q /S dist")
+for f in os.listdir(os.getcwd()):
+    if f.startswith("warn") or f.startswith("logdict"):
+        os.remove(f)
 
--- scripts/builder_OSX.sh
+++ scripts/builder_OSX.sh
@@ -7,7 +7,7 @@ if [ -f setup.py ]; then
     mv setup.py setup_back.py;
 fi
 
-py2applet --make-setup Cecilia5.py Resources/*
+py2applet --make-setup --argv-emulation=0 Cecilia5.py Resources/*
 python setup.py py2app --plist=scripts/info.plist
 rm -f setup.py
 rm -rf build
@@ -26,18 +26,18 @@ fi
 rm Cecilia5.app/Contents/Resources/Cecilia5.ico
 rm Cecilia5.app/Contents/Resources/CeciliaFileIcon5.ico
 
-ditto --rsrc --arch i386 Cecilia5.app Cecilia5-i386.app
+# keep only 64-bit arch
+ditto --rsrc --arch x86_64 QLive.app QLive-x86_64.app
+rm -rf QLive.app
+mv QLive-x86_64.app QLive.app
+
+ditto --rsrc --arch x86_64 Cecilia5.app Cecilia5-x86_64.app
 rm -rf Cecilia5.app
-mv Cecilia5-i386.app Cecilia5.app
+mv Cecilia5-x86_64.app Cecilia5.app
 
 cd ..
 cp -R Cecilia5_OSX/Cecilia5.app .
 
-# Fixed wrong path in Info.plist
-cd Cecilia5.app/Contents
-awk '{gsub("Library/Frameworks/Python.framework/Versions/2.6/Resources/Python.app/Contents/MacOS/Python", "@executable_path/../Frameworks/Python.framework/Versions/2.6/Python")}1' Info.plist > Info.plist_tmp && mv Info.plist_tmp Info.plist
-
-cd ../..
 echo "assembling DMG..."
 mkdir "$DMG_DIR"
 cd "$DMG_DIR"
